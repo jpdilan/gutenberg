c12ca1afacc3fb2e1998807fe6d4c825a3dc7ea6
Dilan Nilaksha <jp.dilan@gmail.com>
2018-08-25 17:33:48 +0530

    Update custom_post.js

--- custom_post.js
+++ custom_post.js
@@ -1,7 +1,36 @@
 
 var todoList = {
 
+todo:[],
 
+displayTodo:function(){
+		console.log(this.todo);
+	},
+
+addTodo:function(todoText){
+	this.todo.push({
+					todoText:todoText,
+					complete:false 
+					});
+
+	this.displayTodo();
+	},
+
+updateTodo:function(position,todoText){
+	this.todo[position].todoText=todoText;
+	this.displayTodo();
+	},
+
+deleteTodo:function(position){
+	this.todo.splice(position,1);
+	this.displayTodo();
+	},
+
+toggleTodo:function(position){
+		var TODO = this.todo[position];
+		TODO.complete=!TODO.complete;
+		this.displayTodo();			
+	}
 
 
 
